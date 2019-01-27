35a119822f07a1787d3e3b08d06ef87c778ff7bd
Razvan <rainertpaypal@yahoo.com>
2019-01-27 13:33:29 +0200

    Bogdanele fii atent aici mai tata mai

--- src/main/java/Darts.java
+++ src/main/java/Darts.java
@@ -1,3 +1,5 @@
+//branch DARTS_INITIAL
+
 public class Darts {
     enum Multiplier {
         DOUBLE, TRIPLE, XX;
@@ -8,6 +10,26 @@ public class Darts {
     private boolean isFinished = false;
     private int turn = 1;
     private int lastTurnScore = score;
+
+    public int score() {
+        return score;
+    }
+
+    public int dartsLeft() {
+        return dartsLeft;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public int turn() {
+        return turn;
+    }
+
+    public int lastTurnScore() {
+        return lastTurnScore;
+    }
 }
 
 //Darts class in MASTER
