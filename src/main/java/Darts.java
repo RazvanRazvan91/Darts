691044ff3e7bfc86c2376f00ecb88b7dfb9feb3f
= <=>
2019-01-27 13:37:02 +0200

    second comit

--- src/main/java/Darts.java
+++ src/main/java/Darts.java
@@ -1,3 +1,5 @@
+//Darts class in MASTER
+
 public class Darts {
     enum Multiplier {
         DOUBLE, TRIPLE, XX;
@@ -9,5 +11,51 @@ public class Darts {
     private int turn = 1;
     private int lastTurnScore = score;
 }
+    public void darts(int throwScore) {
+        updateDartBoard(throwScore, null);
+    }
 
-//Darts class in MASTER
+    public void darts(int i, Multiplier m) {
+        int multiplier;
+        int throwScore;
+        switch (m) {
+            case DOUBLE:
+                multiplier = 2;
+                break;
+            case TRIPLE:
+                multiplier = 3;
+                break;
+            default:
+                multiplier = 1;
+                break;
+        }
+        throwScore = multiplier * i;
+        updateDartBoard(throwScore, m);
+    }
+
+    private void updateDartBoard(int throwScore, Multiplier m) {
+
+        int currentScore = score - throwScore;
+
+        if (dartsLeft == 1) {
+            turn++;
+            dartsLeft = 3;
+        } else {
+            dartsLeft--;
+        }
+
+        if (currentScore == 0 && m == Multiplier.DOUBLE) {
+            isFinished = true;
+        } else {
+            if (currentScore > 1 && dartsLeft == 3) {
+                lastTurnScore = currentScore;
+            }
+            if (currentScore <= 1) {
+                currentScore = lastTurnScore;
+
+            }
+        }
+        score = currentScore;
+    }
+
+}
